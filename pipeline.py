b452e5ee21f3192c5deffbf80f0a3b7149045300
Ramcharan-AIML <yachamaneniramcharan31@gmail.com>
2026-08-30 16:33:45 +0530

    pipeline is completed

--- pipeline.py
+++ pipeline.py
@@ -5,6 +5,8 @@ def run_research_pipeline(topic : str) -> dict:
 
     state = {}
 
+    # Step - 1 Search Agent
+
     # Search Agent working
     print("\n" + "=" * 50)
     print("step 1 - search agent is working ...")
@@ -20,5 +22,63 @@ def run_research_pipeline(topic : str) -> dict:
     print("\n search result ", state['search_results'])
 
 
+    # Step - 2 Reader Agent
 
+    print("\n" + "=" * 50)
+    print("step 2 - Reader agent is scraping top resources ...")
+    print("=" * 50)
+
+    reader_agent = build_reader_agent()
+    reader_result = reader_agent.invoke({
+        "messages" : [("user",
+            f"Based on the following search results about '{topic}', "
+            f"pick the most relevant URL and scrape it for deeper content.\n\n"
+            f"Search results:\n {state['search_results'][:800]}"
+        )]
+    })
+
+    state["scraped_content"] = reader_result['messages'][-1].content
+
+    print("\n scraped content", state['scraped_content'])
+
+
+
+    # Step 3 -  Writer Chain
+
+    print("\n" + "=" * 50)
+    print("step 2 - Writer drafting the report...")
+    print("=" * 50)
+
+    research_combined = (
+        f"SEARCH RESULTS : \n {state['search_results']} \n\n"
+        f"DETAILED SCRAPED CONTENT : \n {state['scraped_content']}"
+    )
+
+    state["report"] = writer_chain.invoke({
+        "topic" : topic,
+        "research" : research_combined
+    })
+
+    print("\n Final Report\n", state['report'])
+
+
+    # Step 4 - Critic Chain
+
+    print("\n" + "=" * 50)
+    print("step 2 - Critic is reviewing the report...")
+    print("=" * 50)
+
+    state["feedback"] = critic_chain.invoke({
+        "report" : state['report']
+    })
+
+    print("\n critic report \n ", state['feedback'])
+
+    return state 
+
+
+
+if __name__ == "__main__":
+    topic = input("\n Enter a research topic: ")
+    run_research_pipeline(topic)
 
